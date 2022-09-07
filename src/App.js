c687dfe5d5d4350be48f9128b0466704650ce66e
Irene Wang <ireneziwang@gmail.com>
2022-09-06 20:17:50 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Irene Wang</h1>
+       <p>Software Developer</p>
+       <button>LinkedIn</button>
+       <button>GitHub</button>
+
+      </header>
+
+      <div className="card-work">
+        <h2>Information</h2>
+      </div>
+
+      <div className="card-education">
+        <h2>Education</h2>
+          <h3>University of California, Los Angeles</h3>
+      </div>
+      <div className="card-projects">
+        <h2>Projects</h2>
+      </div>
+
+      <div className="card-skills">
+       <h2>Skills</h2>
+      </div>
+
+      <div className="card-work">
+        <h2>Work experience</h2>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
